e78471f38b39970adbc21cdf0ed12a10d70d489d
Umer Faiz <umer.faiz@10pearls.com>
2022-10-07 04:52:13 +0500

    routes basics

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import logo from './logo.svg';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-      <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
+
   );
 }
 
