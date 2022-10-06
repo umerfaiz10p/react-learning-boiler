e7766c4e8595fd7771d0fe42d955cf5105ca171b
Umer Faiz <umer.faiz@10pearls.com>
2022-10-07 02:44:14 +0500

    props and states

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,16 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-  const [name, setName] = useState('umer');
-  const [age, setAge] = useState(25);
-
-  const handleClick = () => {
-    setName('umer 10P');
-    setAge(30);
-  }
+  const [blogs, setBlogs] = useState([
+    { title: 'React blog 1', body: 'lorem ipsum...', author: 'umer', id: 1 },
+    { title: '10P game Fest!', body: 'lorem ipsum...', author: 'faiz', id: 2 },
+    { title: 'React learning FS!', body: 'lorem ipsum...', author: '10P', id: 3 }
+  ])
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>{ name } is { age } years old</p>
-      <button onClick={handleClick}>Click me</button>
+      <BlogList blogs={blogs} title="All Blogs" />
     </div>
   );
 }
