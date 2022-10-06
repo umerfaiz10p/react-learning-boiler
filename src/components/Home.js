baf567d1df0bebb89219a8da582150995c1e4ce0
Umer Faiz <umer.faiz@10pearls.com>
2022-10-07 03:13:53 +0500

    use Effect hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -7,16 +7,25 @@ const Home = () => {
     { title: '10P game Fest!', body: 'lorem ipsum...', author: 'faiz', id: 2 },
     { title: 'React learning FS!', body: 'lorem ipsum...', author: '10P', id: 3 }
   ])
-  
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
   }
 
+  let [count, setName] = useState(0);
+
+  useEffect(() => {
+    console.log('use effect ran');
+  }, [count])
+
   return (
     <div className="home">
+      <p>Number of UseEffect test is {count}</p>
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName(++count)}>Test UseEffect hook</button>
       <BlogList blogs={blogs.filter(blog => blog.author === 'umer')} title="Umer's Blogs" handleDelete={handleDelete}/>
+
     </div>
   );
 }
