2a50d9121f020fc6d08592b0bb4d3aec50ff9289
Umer Faiz <umer.faiz@10pearls.com>
2022-10-07 03:51:25 +0500

    conditional loading message

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,19 +3,23 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null)
-
+  const [isPending, setIsPending] = useState(true);
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
       .then(res => {
         return res.json();
       })
       .then(data => {
+        setIsPending(false);
         setBlogs(data);
       })
+    }, 1000);
   }, [])
 
   return (
     <div className="home">
+      { isPending && <div>Loading...</div> }
       {blogs && <BlogList blogs={blogs} />}
       {blogs && <BlogList blogs={blogs.filter(blog => blog.author === 'umer')} title="Umer's Blogs"/>}
     </div>
